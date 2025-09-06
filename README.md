3e2049f2d90ec7b593c2bacfd1374cd9f419afb5
Kirill Voinov <kurillviynov@gmail.com>
2025-09-06 11:27:25 +0300

    Added main functions:  - for making habits  - for getting info about habit  - for notifications (future dev)  - for makring habit is done
    Also wrote README.md file with info about functions and futute of the project.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# hibit_tracker
-My first project on Python. Helps to track my habits.
+# Habit Tracker
+Helps to track my habits. My first project on Python. 
+
+## Functions
+1. User can add habits which they want to track. He need to write:
+    - name of the habit.
+    - discrition of the habit (the accurate discription what he or she should do to be sure he keep doing this habit).
+
+2. User can get information about habit:
+    - how many days user keep doing it.
+    - last time user did it.
+    - discription of the habit.
+
+3. User can get notifications about his current everyday habits. They get:
+    - name of the habit.
+    - discription what to do.
+
+4. User can mark the habit as done. They should only enter the name of the habit.
+
+## Future development
+I think this project will be deployed via Telegram bot to use on your smartphones.
---
+++ app.py
@@ -0,0 +1,70 @@
+import json
+
+def add_habit(name_of_habit):
+    '''
+    Get the name of the habit wanted to be tracked
+
+    Args:
+    name_of_habit (str): name of habit
+
+    Return:
+    ...
+
+    Raise:
+    ...
+
+    to be continued...
+    '''
+    pass
+
+def mark_done(name_of_habit):
+    '''
+    Mark habit as done
+
+    Args:
+    name_of_habit (str): name of habit
+
+    Return:
+    ... (mb text with successful process)
+
+    Raise:
+    ...
+
+    to be continued...
+    '''
+    pass
+
+def notify():
+    '''
+    Send notifications to user
+
+    Args:
+    ...
+
+    Return:
+    ... (mb text with successful process)
+
+    Raise:
+    ...
+
+    to be continued...
+    '''
+    pass
+
+def get_info(name_of_habit):
+    '''
+    Gives the discription, last date when it was done and streak of \
+        the habit tracked
+
+    Args:
+    name_of_habit (str): name of habit
+
+    Return:
+    ... (mb text with successful process)
+
+    Raise:
+    ...
+
+    to be continued...
+    '''
+    pass
